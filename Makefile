01426859aac6f2c6df2a34013c224485e70e6be8
Alexander Solovyov <alexander@solovyov.net>
2021-04-27 11:34:07 +0300

    upd deps

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	clj -M:cider
+
+ancient:
+	clj -M:cider:ancient
---
+++ dev/user.clj
@@ -0,0 +1,7 @@
+(ns user
+  (:require [mount.core :as mount]
+
+            [ecomspark.main]))
+
+(def start mount/start)
+(def stop mount/stop)
